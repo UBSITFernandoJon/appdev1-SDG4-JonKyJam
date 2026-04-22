3b73b45436e1fb2d080f6679ca34dee3d15a7731
UBSITPagnasJames <20227106@s.ubaguio.edu>
2026-04-22 13:09:49 +0800

    Added The Css for the Dashboard and About Page, Also We added About Us Description

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -9,6 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './about.css'
 })
 export class AboutComponent {
+  AboutUs = [
+    'We are a group of students from Baguio City, proudly known as JONKYJAM. As learners ourselves, we understand the importance of accessible and reliable educational resources in supporting academic growth.',
+    'Through this platform, we aim to help students enhance their knowledge, improve their learning experience, and develop a stronger passion for education. JONKYJAM is built with dedication, teamwork, and a shared goal of making learning more accessible for all.',
+    'We are committed to continuously improving our website and expanding our collection of educational resources to better serve students and educators.'
+  ];
+
+
   Mission = [
     'As JONKYJAM, a group of students from Baguio City, our mission is to create and maintain an educational website that offers free access to books and learning materials. We aim to support students in their studies by making education more convenient, inclusive, and available anytime and anywhere.'
 
